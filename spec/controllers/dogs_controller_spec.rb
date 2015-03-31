62f17e8e988f727ddd0f10562ccf6fb18e2959cb
Sydnee Stanley <s.stanley@berkeley.edu>
2015-03-31 14:16:25 -0700

    added dog routes and add dog button

---
+++ spec/controllers/dogs_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe DogsController, :type => :controller do
+  describe 'add dog profile' do
+    before(:each) do
+      #add factory girl for person here
+
+    end
+  end
+end
