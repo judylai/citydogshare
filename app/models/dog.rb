503a34e905f5a9ad244c456969f42b71b9184187
aimichelle <aimichelle@berkeley.edu>
2015-04-02 02:33:31 -0700

    age calculation done

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -11,6 +11,14 @@ class Dog < ActiveRecord::Base
   belongs_to :energy_level
   belongs_to :size
 
+  def age
+    now = Time.now.utc.to_date
+    now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+  end
 
+  # def personalities
+  #   #personalities
+  #   #personalities.map{|personality| personality.name}
+  # end
 
 end
--- db/migrate/20150329142250_rename_tables.rb
+++
@@ -1,6 +0,0 @@
-class RenameTables < ActiveRecord::Migration
-  def change
-  	rename_table :energy_level, :energy_levels
-  	rename_table :personality, :personalities
-  end
-end
