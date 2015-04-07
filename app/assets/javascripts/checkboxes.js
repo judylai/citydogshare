e10dfe5915f1dfde5afc45f330810e0ada25f534
Alex Greenspan <greenspan.alex@yahoo.com>
2015-04-07 00:00:20 -0700

    fixed user profiles, checkboxes, footer

--- app/assets/javascripts/checkboxes.js
+++ app/assets/javascripts/checkboxes.js
@@ -1,21 +1,24 @@
 $(document).ready(function(){
 
   $.each($(':checked'), function( index, value ) {
-    $(value).parent().addClass('active');
+    $(value).parent().addClass('black inverted active');
   });
 
   $('.ui.button').on('click', function(){
-    $(this).toggleClass('active');
-    var $this_checkbox = $(this).children();
-    if ($this_checkbox.is(':checked')){
-      $this_checkbox.prop('checked', false);
-    } else {
-      $this_checkbox.prop('checked', true);
-    }
+    // if($(this).is('#dog_search_submit')) {
+      // console.log("HEEEEE");
+      $(this).toggleClass('black inverted active');
+      var $this_checkbox = $(this).children();
+      if ($this_checkbox.is(':checked')){
+        $this_checkbox.prop('checked', false);
+      } else {
+        $this_checkbox.prop('checked', true);
+      }
+    // }
   });
 
-  $('.check').on('click', function(){
-    $(this).parent().toggleClass('active');
+  $('.check').not( "#dog_search_submit" ).on('click', function(){
+    $(this).parent().toggleClass('black inverted active');
   });
 
 });
