feebb215318f8eadf9dd62e8a84686be1f1a68ea
Sydnee Stanley <s.stanley@berkeley.edu>
2015-04-12 23:30:33 -0700

    trying to fix heroku: precompile tags

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,7 @@ Citydogshare::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
+  config.assets.precompile += %w( tags.js )
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
