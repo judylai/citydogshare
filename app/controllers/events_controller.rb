0a810c20a363fc40fb8ff71b962f1a0a1f269adf
Alex Greenspan <greenspan.alex@yahoo.com>
2015-04-16 16:46:10 -0700

    updated event route and controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,17 +1,27 @@
 class EventsController < ApplicationController
   before_filter :current_user
 
+  def index
+
+  end
+
   def new
     @times = ["Morning", "Afternoon", "Evening", "Overnight"]
     @checked_times = []
   end
 
+  def create
+
+  end
 
   def edit
     @event = Event.find(params[:id])
     @dog = @event.dog_id
   end
 
+  def update
+
+  end
 
   def destroy
 
