e9cdcfd4e88ab92b83386e1ccf6a2f7f7a31f188
aimichelle <aimichelle@berkeley.edu>
2015-04-25 23:08:49 -0700

    precompiled js

--- config/application.rb
+++ config/application.rb
@@ -59,7 +59,8 @@ module Citydogshare
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
+    
     config.assets.precompile << %w( galleria-1.4.2.js )
+    config.assets.compile = true
   end
 end
